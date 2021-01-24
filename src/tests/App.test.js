55a2ee75666e18d1a5bc712af0a01ee28fd24726
Zak Zangrando <48595141+zakzc@users.noreply.github.com>
2021-01-24 13:49:53 +0100

    Initil test implementations

---
+++ src/tests/App.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Enzyme from "enzyme";
+// import ReactDOM from "react-dom";
+// import { render } from "@testing-library/react";
+///
+import Adapter from "enzyme-adapter-react-16";
+import { shallow, configure } from "enzyme";
+///
+import App from "../App";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("App tests", () => {
+  const app = shallow(<App />);
+
+  it("Renders the app", () => {
+    expect(app).toMatchSnapshot();
+  });
+
+  it("Initializes the State", () => {
+    expect(app.state().userIsLoggedIn).toEqual(false);
+    expect(app.state().currentUser).toEqual("");
+    expect(app.state().currentStack).toEqual("");
+  });
+});
