2dbda8a67dbd7b414ccbccd840be705815b7b71b
Zak Zangrando <48595141+zakzc@users.noreply.github.com>
2021-02-07 19:51:06 +0100

    Consolidated and built

---
+++ build/precache-manifest.811646a141ac42fed0ffa28944916709.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce07d80d25014eeb94904fd317ea1b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bafaf8b1f8a8a15a08f4",
+    "url": "/static/css/main.91d7079c.chunk.css"
+  },
+  {
+    "revision": "8d81db313452e629ae2a",
+    "url": "/static/js/2.c0338130.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.c0338130.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bafaf8b1f8a8a15a08f4",
+    "url": "/static/js/main.26ff1319.chunk.js"
+  },
+  {
+    "revision": "0338c2c4181def7758c7",
+    "url": "/static/js/runtime-main.c14297b8.js"
+  }
+]);
